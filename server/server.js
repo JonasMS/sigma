0ac78e3e87739c93db5ca36ce4831f6868adaf6c
JonasMS <jonas.sota@gmail.com>
2016-08-25 17:15:12 -0700

    FEAT - Read and Write outbox.json on GET & POST to /outbox

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
 const api = require('./api.js');
 
 app
   .use(express.static(path.join(__dirname, 'views')))
   .use(express.static(path.join(__dirname, '..', 'build')))
-  .use(api);
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use('/', api);
 
-  console.log(path.join(__dirname, '..', 'build'));
 
 app.listen(3000);
 console.log('server listening on port 3000');
